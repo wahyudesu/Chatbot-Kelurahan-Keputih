953b9751990fd49d56141a8bb96b922af65c4d38
fawzan745 <fawzaaninsani@gmail.com>
2024-09-30 14:29:25 +0700

    update

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,31 @@
+import os
 import requests
 import telebot
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
 
-API_TOKEN = "7804271051:AAGGTqTy2ApgIb7zqdCnCFjATCyRFEkB0LQ"
+# Load environment variables from .env file
+load_dotenv()
+
+# Get tokens from environment variables
+API_TOKEN = os.getenv("API_TOKEN")
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": f"Bearer {HUGGINGFACE_TOKEN}"}
 bot = telebot.TeleBot(API_TOKEN)
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 def read_excel_to_dict(file_path):
-  df = pd.read_excel(file_path)
-  df = df.drop(columns=['No'])
-  data_dict = dict(zip(df.iloc[:, 0], df.iloc[:, 1]))
-  return data_dict
+    df = pd.read_excel(file_path)
+    df = df.drop(columns=['No'])
+    data_dict = dict(zip(df.iloc[:, 0], df.iloc[:, 1]))
+    return data_dict
 
 file_path = 'Data/Data Informasi.xlsx'
 data = read_excel_to_dict(file_path)
@@ -27,11 +34,7 @@ sentences = list(data.keys())
 # Define a command handler
 @bot.message_handler(commands=['start', 'mulai'])
 def send_welcome(message):
-	bot.reply_to(message, "Selamat datang di chatbot keputih, ada yang bisa saya bantu?")
-
-# @bot.message_handler(commands=['info'])
-# def send_info(message):
-# 	bot.reply_to(message, "Ini adalah telegram bot untuk kebutuhan kepengurusan dokumen kelurahan keputih.")
+    bot.reply_to(message, "Selamat datang di chatbot keputih, ada yang bisa saya bantu?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
